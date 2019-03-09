c2534b03979a855e0cf70916d9de34f938ff698f
李章芬 <lizhangfen@geyeclouds.com>
2019-03-09 08:56:53 +0800

    initial commit

---
+++ lzfblog/lzfblog/settings/develop.py
@@ -0,0 +1,13 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
